50a27c6b501c47a9b8d650c12c2c2f04c7540a9e
PCuser <1809139837@qq.com>
2023-07-09 11:53:41 +0800

    第2次提交

--- src/main/java/com/example/shop/ShopApplication.java
+++ src/main/java/com/example/shop/ShopApplication.java
@@ -1,14 +1,40 @@
 package com.example.shop;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
-public class ShopApplication {
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+@Configuration //配置代码
+@SpringBootApplication  //项目启动类
+@MapperScan("com.example.shop.mapper")// mybatis管辖范围
+public class ShopApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
-
         SpringApplication.run(ShopApplication.class, args);
+
+        System.out.println("(♥◠‿◠)ﾉﾞ  后台启动成功   ლ(´ڡ`ლ)ﾞ  \n" );
+    }
+
+
+
+//    @Bean
+//    public MybatisPlusInterceptor page() {
+//        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+//        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+//        return interceptor;
+//    }
+
+    @Override //虚拟路径：上传图片成功后，要用，将未显示的图片显示出来
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/shop/**").addResourceLocations("file:F:/create/shop/");
     }
 
 }
---
+++ src/main/java/com/example/shop/bean/UserBean.java
@@ -0,0 +1,27 @@
+package com.example.shop.bean;
+//普通类--数据表
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //生成get set
+//set get -> freemarker选入按数据时必须要用
+@AllArgsConstructor
+@NoArgsConstructor
+
+@TableName("tbl_user")
+public class UserBean {
+    //
+    @TableId(type = IdType.AUTO) //id自动递增，与数据表对应
+    public Integer id; //不用int，integer包装类，可接收Null
+    public String username;
+    public String password;
+    public String user;
+    public String status;
+    public String store;
+
+}
