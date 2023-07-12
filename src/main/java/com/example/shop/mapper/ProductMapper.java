f63d24c24ce9509a036c6a4d718032fa855d4a66
liwang <liwang.@qq.com>
2023-07-12 20:44:21 +0800

    pinglun

--- src/main/java/com/example/shop/mapper/ProductMapper.java
+++ src/main/java/com/example/shop/mapper/ProductMapper.java
@@ -4,7 +4,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.shop.bean.ProductBean;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+
 import java.util.List;
+
 
 public interface ProductMapper extends BaseMapper<ProductBean> {
 
